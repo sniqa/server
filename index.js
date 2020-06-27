f84bf1e516ed6a98e821a4eb7cecba0f26a63095
sniqa <sniqasystem@outlook.com>
2020-06-27 21:45:36 +0800

    6/27

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const Koa = require('koa')
-const koaBody = require('koa-body')
-const path = require('path')
+const Koa = require('koa');
+const app = new Koa();
+const Router = require('koa-router');
+const multer = require('koa-multer');
+const serve = require('koa-static');
+const path = require('path');
+const fs = require('fs-extra');
+const koaBody = require('koa-body');
 const cors = require('koa2-cors')
-const Mongoose = require('mongoose')
-const Router = require('koa-router')
 
-const app = new Koa()
-const staticFiles = require('koa-static')
-
-
-
-
-app.use(staticFiles(path.resolve(__dirname, "./dist")))
-
-
-
-
-
-//导入 cors
+const {
+  mkdirsSync
+} = require('./utils/dir');
+const uploadPath = path.join(__dirname, 'uploads');
+const uploadTempPath = path.join(uploadPath, 'temp');
+const upload = multer({
+  dest: uploadTempPath
+});
+const router = new Router();
+app.use(koaBody());
 app.use(cors({
   origin: (ctx) => {
     if (ctx.url === 'test') {
@@ -31,88 +31,66 @@ app.use(cors({
   allowMethods: ['GET', 'post', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
   allowHeaders: ['Content-Type', 'Authorization', 'Accept', '*']
 }))
-//end of cors
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.use(serve(__dirname + '/static'));
+/**
+ * single(fieldname)
+ * Accept a single file with the name fieldname. The single file will be stored in req.file.
+ */
+router.post('/file/upload', upload.single('file'), async (ctx, next) => {
+  console.log('file upload...')
+  // 根据文件hash创建文件夹，把默认上传的文件移动当前hash文件夹下。方便后续文件合并。
+  const {
+    name,
+    total,
+    index,
+    size,
+    hash
+  } = ctx.req.body;
 
-app.use(koaBody())
-
-
-/**********  使用路由 ********/
-const route = new Router()
-app.use(route.routes()).use(route.allowedMethods())
-
-//连接MongoDB
-// const {
-//   mongodbStr
-// } = require('./schema/user/DB_login_cfg')
-// Mongoose.connect(mongodbStr, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// }, () => {
-//   console.log('MongoDB connect successful')
-// })
-// Mongoose.connection.on('error', console.error)
-//end of MongoDB
-
-
-
-
-
-
-
-route.get('/', async (ctx) => {
-  ctx.body = 'hello world'
+  const chunksPath = path.join(uploadPath, hash, '/');
+  if (!fs.existsSync(chunksPath)) mkdirsSync(chunksPath);
+  fs.renameSync(ctx.req.file.path, chunksPath + hash + '-' + index);
+  ctx.status = 200;
+  ctx.res.end('Success');
 })
 
+router.post('/file/merge_chunks', async (ctx, next) => {
+  const {
+    size,
+    name,
+    total,
+    hash
+  } = ctx.request.body;
+  // 根据hash值，获取分片文件。
+  // 创建存储文件
+  // 合并
+  const uploadPath = path.join(__dirname, 'uploads');
 
-/*************** phl ************/
-const UsersCtl = require('./test/index')
-
-route.post('/phl', async (ctx) => {
-  const request = ctx.request.body
-  let response = request
-  for (var func in request) {
-    // console.log(Array.isArray(request[func]))
-    if (!Array.isArray(request[func])) {
-      response[func] = await UsersCtl[func](request[func])
-    } else {
-      const arrTemp = []
-      for (var args in request[func]) {
-
-        console.log(request[func][args])
-        const user = await UsersCtl[func](request[func][args])
-        arrTemp.push(user)
-      }
-      response[func] = arrTemp
-    }
-
-
-    // for(var value in request[key])  
-    //   console.log(typeof(request[key]));
-
-
-    ctx.body = response[func]
+  const chunksPath = path.join(uploadPath, hash, '/');
+  const filePath = path.join(uploadPath, name);
+  // 读取所有的chunks 文件名存放在数组中
+  const chunks = fs.readdirSync(chunksPath);
+  // 创建存储文件
+  fs.writeFileSync(filePath, '');
+  if (chunks.length !== total || chunks.length === 0) {
+    ctx.status = 200;
+    ctx.res.end('切片文件数量不符合');
+    return;
   }
+  for (let i = 0; i < total; i++) {
+    // 追加写入到文件中
+    fs.appendFileSync(filePath, fs.readFileSync(chunksPath + hash + '-' + i));
+    // 删除本次使用的chunk    
+    fs.unlinkSync(chunksPath + hash + '-' + i);
+  }
+  fs.rmdirSync(chunksPath);
+  // 文件合并成功，可以把文件信息进行入库。
+  ctx.status = 200;
+  ctx.res.end('合并成功');
 })
 
-const iphl = require('./dist/index')
-route.get('/iphl', (ctx) => {
-  ctx.body = iphl
-})
-
-route.post('/test', async (ctx) => {
-  ctx.body = 'hello world'
-})
-
-
-
-
-
-
-
-
-//监听端口
-app.listen({
-  port: 8000
-}, () => {
-  console.log(`server ready at http://localhost:8000`)
-})
+app.listen(9000, () => {
+  console.log('服务9000端口已经启动了');
+});
